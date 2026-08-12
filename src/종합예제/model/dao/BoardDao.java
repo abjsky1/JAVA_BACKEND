0d8542fb7d80a2ba2742bc62ca71f3bd9076a054
abjsky1 <abjsky1@gmail.com>
2026-08-12 16:06:05 +0900

    JAVA_종합예제

--- src/종합예제/model/dao/BoardDao.java
+++ src/종합예제/model/dao/BoardDao.java
@@ -10,6 +10,7 @@ public class BoardDao implements IBaseDao {
     private ArrayList<Object> boardList = new ArrayList<>();
 
     // TODO 1: IBaseDao의 save() 메서드를 오버라이딩하여 boardList 저장하는 로직 작성
+    
 
     // TODO 2: IBaseDao의 findAll() 메서드를 오버라이딩하여 boardList를 반환하는 로직 작성
     
